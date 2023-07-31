265a704a14bf74ddc274e030a1dd9fad6def8c36
menon <MENON@ITAIPU.GOV.BR>
2023-07-31 09:57:28 -0300

    Script SCV incluso

---
+++ changeSCVLink.ps1
@@ -0,0 +1,72 @@
+# changeSvcLink para SSA 202312444
+# Autor: Mauricio Menon
+# Versão 1.0 31/07/2023
+# Desenvolvido para PowerShell 5.1
+
+Start-Transcript -Path $logFile     #-Append
+
+# Definicao de lista de consoles 
+$ccrConsoleList = @('bitcon1', 'bitcon2', 'bitcon3')
+$desConsoleList = @('bitcon11', 'bitcon12', 'bitcon13')
+
+$allConsoles = $ccrConsoleList + $desConsoleList
+
+function Test-AdminPrivilege {
+    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    if (-not $isAdmin) {
+        Write-Host 'Este script deve ser executado com privilegios de administrador.'
+    }
+    else {
+        write-warning 'Usuario Administrador'
+    }
+}
+
+function Get-Environment {
+    $domain = $env:USERDNSDOMAIN
+    $domain = $domain.ToLower()
+
+    if ($domain -match 'ems') {
+        return "ems"
+    }
+    elseif ($domain -match 'itaipu') {
+        # para criar lista para a máquina local no caso de debug do script
+        return "itaipu"                            
+    }
+    else {
+        return "Dominio nao pertencente ao EMS-SCADA"
+    }
+}
+
+foreach ($console in $allConsoles) {
+    Write-Output "Processando console $console"
+
+    # Alterando o caminho do aplicativo SCV
+    $shortcutPath = Join-Path -Path $console -ChildPath "c:\aplicativos\scv\bin\SCV.lnk"
+    if (Test-Path -Path $shortcutPath) {
+        $shell = New-Object -ComObject WScript.Shell
+        $shortcut = $shell.CreateShortcut($shortcutPath)
+        $shortcut.TargetPath = "\\bitaps1\scv\bin"
+        $shortcut.Save()
+        Write-Output "Link do aplicativo SCV alterado com sucesso em $console"
+    }
+    else {
+        Write-Output "Link do aplicativo SCV nao encontrado em $console"
+    }
+
+    # Removendo as pastas
+    $foldersToRemove = @("c:\aplicativos\scv\bin", "c:\aplicativos\scv\Xml")
+    foreach ($folder in $foldersToRemove) {
+        $folderPath = Join-Path -Path $console -ChildPath $folder
+        if (Test-Path -Path $folderPath) {
+            Remove-Item -Path $folderPath -Recurse -Force
+            Write-Output "Pasta $folder removida com sucesso de $console"
+        }
+        else {
+            Write-Output "Pasta $folder nao encontrada em $console"
+        }
+    }
+}
+
+
+
+Stop-Transcript
