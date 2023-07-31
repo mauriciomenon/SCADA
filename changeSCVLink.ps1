cb98a43f072e7d6e7bf4fc858c40fc6adab80481
menon <MENON@ITAIPU.GOV.BR>
2023-07-31 10:54:51 -0300

    Alterado diretorio do usuario - Desktop

--- changeSCVLink.ps1
+++ changeSCVLink.ps1
@@ -6,19 +6,18 @@
 $logFile = Join-Path -Path (Get-Location).Path -ChildPath "logfile.txt"
 Start-Transcript -Path $logFile
 
-# Definicao de lista de consoles 
+# Definicao de lista de consoles do CCR e Despacho
 $ccrConsoleList = @('localhost','bitcon1', 'bitcon2', 'bitcon3')
 $desConsoleList = @('bitcon11', 'bitcon12', 'bitcon13')
-
 $allConsoles = $ccrConsoleList + $desConsoleList
 
 function Test-AdminPrivilege {
     $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
     if (-not $isAdmin) {
-        Write-Host 'Este script deve ser executado com privilegios de administrador.'
+        Write-Host 'Este script deve ser executado com privilegio de Administrador.'
     }
     else {
-        Write-Warning 'Usuario Administrador'
+        Write-Warning 'Execucao como usuario Administrador'
     }
 }
 
@@ -42,12 +41,12 @@ function Process-Consoles {
     foreach ($console in $allConsoles) {
         Write-Output "Processando console $console"
 
-        # Alterando o caminho do aplicativo SCV
+        # Alterando o link do SCV
         $shortcutPath = Join-Path -Path $console -ChildPath "c:\aplicativos\scv\bin\SCV.lnk"
         if (Test-Path -Path $shortcutPath) {
             $shell = New-Object -ComObject WScript.Shell
             $shortcut = $shell.CreateShortcut($shortcutPath)
-            $shortcut.TargetPath = "\\bitaps1\scv\bin"
+            $shortcut.TargetPath = "\\bitaps1\scv\bin\SCV.exe"
             $shortcut.Save()
             Write-Output "Link do aplicativo SCV alterado com sucesso em $console"
         }
@@ -55,7 +54,7 @@ function Process-Consoles {
             Write-Output "Link do aplicativo SCV nao encontrado em $console"
         }
 
-        # Removendo as pastas
+        # Remover as pastas
         $foldersToRemove = @("c:\aplicativos\scv\bin", "c:\aplicativos\scv\Xml")
         foreach ($folder in $foldersToRemove) {
             $folderPath = Join-Path -Path $console -ChildPath $folder
